70c1f0fffc8f128a540139e4bbd67405b2546dce
aneckar <aneckar@stanford.edu>
2017-03-22 17:59:58 -0700

    Added MutexBuffer
    Wrote some simple tests. Looks like it works. I'm not even sure multiple
    consumers is a use case.

---
+++ src/BDDriver/common/MutexBuffer.cpp
@@ -0,0 +1,123 @@
+#include <assert.h>
+
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+namespace pystorm {
+namespace bddriver {
+
+template<class T>
+MutexBuffer<T>::MutexBuffer(unsigned int capacity)
+{
+  vals_ = new T[capacity];
+  capacity_ = capacity;
+  front_ = 0;
+  back_ = 0;
+  count_ = 0;
+}
+
+template<class T>
+MutexBuffer<T>::~MutexBuffer()
+{
+  delete vals_;
+}
+
+template<class T>
+void MutexBuffer<T>::Push(const T * input, unsigned int input_len)
+{
+  assert(input_len < capacity_ && "trying to insert a vector longer than queue capacity"); // _capacity is TS
+
+  std::unique_lock<std::mutex> lock(push_mutex_);
+
+  // wait for the queue to have enough room for the input
+  //cout << "about to try the lock" << endl;
+  just_popped_.wait(lock, [this, input_len]{ return HasRoomFor(input_len); });
+  //cout << "got the lock" << endl;
+
+  // copy the data
+  for (unsigned int i = 0; i < input_len; i++) {
+    vals_[back_ + i] = input[i];
+  }
+
+  // update queue state
+  back_ = (back_ + input_len) % capacity_;
+  count_ += input_len;
+
+  // let the consumer know it's time to wake up
+  push_mutex_.unlock();
+  just_pushed_.notify_all();
+}
+
+template<class T>
+void MutexBuffer<T>::Push(const std::vector<T>& input)
+{
+  Push(&input[0], input.size());
+}
+
+template<class T>
+unsigned int MutexBuffer<T>::PopN(T * copy_to, unsigned int max_to_pop)
+// copies data from front_ into copy_to. Returns number read (because maybe num_to_read > count_)
+{
+  std::unique_lock<std::mutex> lock(pop_mutex_);
+
+  // wait for the queue to have something to output
+  just_pushed_.wait(lock, [this]{ return !IsEmpty(); });
+
+  // figure out how many elements are going to be in output
+  unsigned int num_popped;
+  if (max_to_pop < count_) {
+    num_popped = max_to_pop;
+  } else {
+    num_popped = count_;
+  }
+
+  // do copy
+  for (unsigned int i = 0; i < max_to_pop; i++) {
+    copy_to[i] = vals_[(front_ + i) % capacity_];
+  }
+
+  // update queue state
+  front_ = (front_ + num_popped) % capacity_;
+  count_ -= num_popped;
+
+  // notify the producer that they may wake up
+  pop_mutex_.unlock();
+  just_popped_.notify_all();
+
+  return num_popped;
+}
+
+
+template<class T>
+std::vector<T> MutexBuffer<T>::PopNVect(unsigned int max_to_pop)
+{
+  std::vector<T> output;
+  output.reserve(max_to_pop);
+
+  unsigned int num_popped = PopN(&output[0]);
+  output.resize(num_popped);
+
+  return output;
+}
+
+template<class T>
+bool MutexBuffer<T>::IsEmpty()
+{
+  return count_ == 0;
+}
+
+template<class T>
+bool MutexBuffer<T>::HasRoomFor(unsigned int size)
+{
+  bool has_room = size <= capacity_ - count_;
+  //cout << "seeing if there's room: " << has_room << endl;
+  return has_room;
+}
+
+} // bddriver
+} // pystorm
---
+++ src/BDDriver/common/MutexBuffer.h
@@ -0,0 +1,51 @@
+#ifndef MUTEXBUFFER_H
+#define MUTEXBUFFER_H
+
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+
+namespace pystorm {
+namespace bddriver {
+ 
+template<class T>
+class MutexBuffer {
+
+  public:
+    MutexBuffer(unsigned int capacity);
+    ~MutexBuffer();
+
+    // fast array interface
+    void Push(const T * input, unsigned int input_len);
+    unsigned int PopN(T * copy_to, unsigned int max_to_pop);
+
+    // vector interface, extra allocation
+    void Push(const std::vector<T> & input);
+    std::vector<T> PopNVect(unsigned int max_to_pop);
+
+  private:
+    T * vals_;
+    unsigned int capacity_;
+    unsigned int front_;
+    unsigned int back_;
+    unsigned int count_;
+
+    // XXX dual mutexes didn't really help because we still need both locks
+    // to check the empty/almost full state
+    std::mutex push_mutex_;
+    std::mutex pop_mutex_;
+    std::condition_variable just_popped_;
+    std::condition_variable just_pushed_;
+
+    bool IsEmpty(); 
+    bool HasRoomFor(unsigned int size);
+
+};
+
+} // bddriver
+} // pystorm
+
+// we have defined a template class. We must include the implementation
+#include "BDDriver/common/MutexBuffer.cpp"
+
+#endif
