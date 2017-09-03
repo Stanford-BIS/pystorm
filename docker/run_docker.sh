aebc1d163f17895c08cbc86b1ebd9b8a6bbf83ea
Ben Varkey Benjamin <benvarkey@users.noreply.github.com>
2017-09-03 13:29:05 -0700

    Test ninja (#41)
    * Change build from Make to Ninja
    
    * bake apt-get into base docker image and fix ninja argument
    
    * ninja -j requires explicit argument
    
    * use ninja -j for test too.

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 HOSTIP=$(ip route show | awk '/default/ {print $3}')
-ssh $HOSTIP "docker run --rm -i $1 make test ARGS=\"-V\""
+ssh $HOSTIP "docker run --rm -i $1 ninja -j 6 test -v"
