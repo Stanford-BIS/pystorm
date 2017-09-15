67f740885b62c84090310c2ecbf0587e1cdeb933
Ben Varkey Benjamin <benvb@clark-boa-tr-33.stanford.edu>
2017-09-14 18:33:10 -0700

    Working on python test script for OK interface

--- src/bindings/python/3.5/PyOK.cpp
+++ src/bindings/python/3.5/PyOK.cpp
@@ -2,6 +2,17 @@
 #include <pybind11/stl.h>
 #include <okFrontPanelDLL.h>
 
+class BD_OK: public okCFrontPanel{
+public:
+    long WriteToBlockPipeIn(unsigned int endpoint, unsigned int block_size, std::vector<uint8_t>& payload){
+        return okCFrontPanel::WriteToBlockPipeIn(
+            static_cast<int>(endpoint),
+            static_cast<int>(block_size),
+            static_cast<long>(payload.size()),
+            static_cast<unsigned char*>(&payload[0]));
+    }
+};
+
 PYBIND11_MODULE(PyOK, m)
 {
     m.def("okFrontPanelDLL_LoadLib", okFrontPanelDLL_LoadLib);
@@ -11,39 +22,40 @@ PYBIND11_MODULE(PyOK, m)
         return std::tuple<std::string, std::string>(lib_date, lib_time);
     });
 
-    pybind11::enum_<okCFrontPanel::ErrorCode>(m, "ErrorCode", "")
-        .value("NoError", okCFrontPanel::ErrorCode::NoError)
-        .value("Failed", okCFrontPanel::ErrorCode::Failed)
-        .value("Timeout", okCFrontPanel::ErrorCode::Timeout)
-        .value("DoneNotHigh", okCFrontPanel::ErrorCode::DoneNotHigh)
-        .value("TransferError", okCFrontPanel::ErrorCode::TransferError)
-        .value("CommunicationError", okCFrontPanel::ErrorCode::CommunicationError)
-        .value("InvalidBitstream", okCFrontPanel::ErrorCode::InvalidBitstream)
-        .value("FileError", okCFrontPanel::ErrorCode::FileError)
-        .value("DeviceNotOpen", okCFrontPanel::ErrorCode::DeviceNotOpen)
-        .value("InvalidEndpoint", okCFrontPanel::ErrorCode::InvalidEndpoint)
-        .value("InvalidBlockSize", okCFrontPanel::ErrorCode::InvalidBlockSize)
-        .value("I2CRestrictedAddress", okCFrontPanel::ErrorCode::I2CRestrictedAddress)
-        .value("I2CBitError", okCFrontPanel::ErrorCode::I2CBitError)
-        .value("I2CNack", okCFrontPanel::ErrorCode::I2CNack)
-        .value("I2CUnknownStatus", okCFrontPanel::ErrorCode::I2CUnknownStatus)
-        .value("UnsupportedFeature", okCFrontPanel::ErrorCode::UnsupportedFeature)
-        .value("FIFOUnderflow", okCFrontPanel::ErrorCode::FIFOUnderflow)
-        .value("FIFOOverflow", okCFrontPanel::ErrorCode::FIFOOverflow)
-        .value("DataAlignmentError", okCFrontPanel::ErrorCode::DataAlignmentError)
-        .value("InvalidResetProfile", okCFrontPanel::ErrorCode::InvalidResetProfile)
-        .value("InvalidParameter", okCFrontPanel::ErrorCode::InvalidParameter)
+    pybind11::enum_<BD_OK::ErrorCode>(m, "ErrorCode", "")
+        .value("NoError", BD_OK::ErrorCode::NoError)
+        .value("Failed", BD_OK::ErrorCode::Failed)
+        .value("Timeout", BD_OK::ErrorCode::Timeout)
+        .value("DoneNotHigh", BD_OK::ErrorCode::DoneNotHigh)
+        .value("TransferError", BD_OK::ErrorCode::TransferError)
+        .value("CommunicationError", BD_OK::ErrorCode::CommunicationError)
+        .value("InvalidBitstream", BD_OK::ErrorCode::InvalidBitstream)
+        .value("FileError", BD_OK::ErrorCode::FileError)
+        .value("DeviceNotOpen", BD_OK::ErrorCode::DeviceNotOpen)
+        .value("InvalidEndpoint", BD_OK::ErrorCode::InvalidEndpoint)
+        .value("InvalidBlockSize", BD_OK::ErrorCode::InvalidBlockSize)
+        .value("I2CRestrictedAddress", BD_OK::ErrorCode::I2CRestrictedAddress)
+        .value("I2CBitError", BD_OK::ErrorCode::I2CBitError)
+        .value("I2CNack", BD_OK::ErrorCode::I2CNack)
+        .value("I2CUnknownStatus", BD_OK::ErrorCode::I2CUnknownStatus)
+        .value("UnsupportedFeature", BD_OK::ErrorCode::UnsupportedFeature)
+        .value("FIFOUnderflow", BD_OK::ErrorCode::FIFOUnderflow)
+        .value("FIFOOverflow", BD_OK::ErrorCode::FIFOOverflow)
+        .value("DataAlignmentError", BD_OK::ErrorCode::DataAlignmentError)
+        .value("InvalidResetProfile", BD_OK::ErrorCode::InvalidResetProfile)
+        .value("InvalidParameter", BD_OK::ErrorCode::InvalidParameter)
         .export_values();
 
-    pybind11::class_<okCFrontPanel>(m, "okCFrontPanel")
+    pybind11::class_<BD_OK>(m, "okCFrontPanel")
         .def(pybind11::init<>())
-        .def("WriteToBlockPipeIn", &okCFrontPanel::WriteToBlockPipeIn)
-        .def("ReadFromBlockPipeOut", &okCFrontPanel::ReadFromBlockPipeOut)
-        .def("OpenBySerial", &okCFrontPanel::OpenBySerial)
-        .def("GetDeviceInfo", &okCFrontPanel::GetDeviceInfo)
-        .def("LoadDefaultPLLConfiguration", &okCFrontPanel::LoadDefaultPLLConfiguration)
-        .def("ConfigureFPGA", &okCFrontPanel::ConfigureFPGA)
-        .def("IsFrontPanelEnabled", &okCFrontPanel::IsFrontPanelEnabled)
+        .def("WriteToBlockPipeIn", (long (BD_OK::*) (unsigned int, unsigned int, std::vector<uint8_t>&)) &BD_OK::WriteToBlockPipeIn)
+        .def("ReadFromBlockPipeOut", &BD_OK::ReadFromBlockPipeOut)
+        .def("ReadFromPipeOut", &BD_OK::ReadFromPipeOut)
+        .def("OpenBySerial", &BD_OK::OpenBySerial)
+        .def("GetDeviceInfo", &BD_OK::GetDeviceInfo)
+        .def("LoadDefaultPLLConfiguration", &BD_OK::LoadDefaultPLLConfiguration)
+        .def("ConfigureFPGA", &BD_OK::ConfigureFPGA)
+        .def("IsFrontPanelEnabled", &BD_OK::IsFrontPanelEnabled)
         ;
 
     pybind11::class_<okTDeviceInfo >(m, "_okTDeviceInfo")
--- test/bindings/python/3_5/OK_test.py
+++ test/bindings/python/3_5/OK_test.py
@@ -1,5 +1,35 @@
-def main():
-    import PyOK as ok
+import sys
+from enum import Enum
+import PyOK as ok
+
+sys.path.append("../../../../lib/")
+sys.path.append("../../../../lib/Release")
+
+ErrorCode = dict({
+      0 : 'NoError'                    ,
+     -1 : 'Failed'                     ,
+     -2 : 'Timeout'                    ,
+     -3 : 'DoneNotHigh'                ,
+     -4 : 'TransferError'              ,
+     -5 : 'CommunicationError'         ,
+     -6 : 'InvalidBitstream'           ,
+     -7 : 'FileError'                  ,
+     -8 : 'DeviceNotOpen'              ,
+     -9 : 'InvalidEndpoint'            ,
+    -10 : 'InvalidBlockSize'           ,
+    -11 : 'I2CRestrictedAddress'       ,
+    -12 : 'I2CBitError'                ,
+    -13 : 'I2CNack'                    ,
+    -14 : 'I2CUnknownStatus'           ,
+    -15 : 'UnsupportedFeature'         ,
+    -16 : 'FIFOUnderflow'              ,
+    -17 : 'FIFOOverflow'               ,
+    -18 : 'DataAlignmentError'         ,
+    -19 : 'InvalidResetProfile'        ,
+    -20 : 'InvalidParameter'
+})
+
+def InitOK(fpga_bitcode="counter.rbf"):
     dev = ok.okCFrontPanel()
     m_devInfo = ok.okTDeviceInfo()
 
@@ -24,7 +54,7 @@ def main():
     print("Device serial number: %s" % m_devInfo.serialNumber)
     print("Device product ID: %d" % m_devInfo.productID)
 
-    if dev.ConfigureFPGA("counters.rbf") != ok.ErrorCode.NoError:
+    if dev.ConfigureFPGA(fpga_bitcode) != ok.ErrorCode.NoError:
         print("FPGA configuration failed.")
         return -3
 
@@ -34,5 +64,90 @@ def main():
         print("FrontPanel support is not enabled")
         return -4
 
+
+class OP():
+
+    BDHORN          = '00'
+    FPGA_REG        = '10'
+    FPGA_CHANNEL    = '11'
+
+    BD_LEAF = Enum('BDLeafId',[
+        'ADC',
+        'DAC0',
+        'DAC10',
+        'DAC11',
+        'DAC12',
+        'DAC1',
+        'DAC2',
+        'DAC3',
+        'DAC4',
+        'DAC5',
+        'DAC6',
+        'DAC7',
+        'DAC8',
+        'DAC9',
+        'DELAY0',
+        'DELAY1',
+        'DELAY2',
+        'DELAY3',
+        'DELAY4',
+        'DELAY5',
+        'DELAY6',
+        'INIT_FIFO_DCT',
+        'INIT_FIFO_HT',
+        'NEURONCONFIG',
+        'NEURONDUMPTOGGLE',
+        'NEURONINJECT',
+        'PROG_AMMM',
+        'PROG_PAT',
+        'PROG_TAT0',
+        'PROG_TAT1',
+        'RI',
+        'TOGGLE_POST_FIFO0',
+        'TOGGLE_POST_FIFO1',
+        'TOGGLE_PRE_FIFO',
+        'INVALID'
+        ], start=0)
+
+    def endpointword(opcode, subcode, payload):
+        _op1 = getattr(OP, opcode)
+        _op2 = None
+        _val = "{0:024b}".format(int(payload, 0))
+        if opcode == 'BDHORN':
+            _op2 = "{0:06b}".format(OP.BD_LEAF[subcode].value)
+        else:
+            _op2 = "{0:06b}".format(int(subcode, 0))
+        return int('0b' + _op1 + _op2 + _val, 0).to_bytes(4, byteorder='little')
+
+def __default_commands__():
+    OPS = [
+        ['BDHORN', 'DAC0', '0x000'],
+        ['BDHORN', 'DAC1', '0x000'],
+        ['BDHORN', 'DAC2', '0x000'],
+        ['BDHORN', 'DAC3', '0x000'],
+        ['BDHORN', 'DAC4', '0x000'],
+        ['BDHORN', 'DAC5', '0x000'],
+        ['BDHORN', 'DAC6', '0x000'],
+        ['BDHORN', 'DAC7', '0x000'],
+        ['BDHORN', 'DAC8', '0x000'],
+        ['BDHORN', 'DAC9', '0x000'],
+        ['BDHORN', 'DAC10', '0x000'],
+        ['BDHORN', 'DAC11', '0x000']
+    ]
+    return [OP.endpointword(*_item) for _item in OPS]
+
+
 if __name__ ==  "__main__":
-    main();
+    import argparse
+    PARSER = argparse.ArgumentParser(description='Program Braindrop with OpalKelly FrontPanel interface')
+    PARSER.add_argument('BITFILE', type=str, help="Bitfile to program the FPGA with")
+    PARSER.add_argument('-f', '--cmdfile', type=argparse.FileType('r'), dest='command_file',
+                        help="File with downstream instructions.\nThis file has one 32-bit word per line.")
+    ARGS = PARSER.parse_args()
+    # `args.BITFILE`        : bitfile
+    # `args.command_file`   : command file
+
+    if ARGS.command_file is None:
+        print("No command file specified. Using default commands...")
+        print(__default_commands__())
+    #InitOK(args.BITFILE)
