1b7971ee354f212650b51a0ed47163257dc06586
Sam Fok <samfok@stanford.edu>
2017-09-19 17:57:30 -0700

    consolidate build requirement checks
    cmake/CheckRequirements now immediately reports a fatal error if
    build environment does not satisfy requirements.
    Previously just set a flag that environment was not valid
    and left it up to including cmake file to report the fatal error
    
    Also eliminates largely unusued HOST and CXX variables.
    cmake already provides variables with equivalent information
    Eliminate redundant variables to reduce potential for errors

--- cmake/CheckRequirements.cmake
+++ cmake/CheckRequirements.cmake
@@ -1,59 +1,28 @@
-# Exports the following variables
-#
-# HOST_IS_LINUX
-# HOST_IS_DARWIN
-# HOST_IS_WINDOWS
-# HOST_IS_VALID
-#
-# CXX_IS_GNU
-# CXX_IS_CLANG
-# CXX_IS_MSVC
-# CXX_IS_VALID
+# Check the build environment
 
 # Check for supported host
 if(NOT CMAKE_HOST_SYSTEM_NAME)
     message(FATAL_ERROR "Uknown host")
 endif()
-
-set(HOST_IS_LINUX OFF)
-set(HOST_IS_DARWIN OFF)
-set(HOST_IS_WINDOWS OFF)
-set(HOST_IS_VALID ON)
-
-if (CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
-    set(HOST_IS_LINUX ON)
+if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
     message(STATUS "Host type is: Linux")
-elseif (CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
-    set(HOST_IS_DARWIN ON)
+elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
     message(STATUS "Host type is: Mac")
-elseif (CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
-    set(HOST_IS_WINDOWS ON)
+elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
     message(STATUS "Host type is: Windows")
-else()
-    set(HOST_IS_VALID OFF)
+else() # Host unsupported
+    message(FATAL_ERROR "Build does not support host '"
+        ${CMAKE_HOST_SYSTEM_NAME} "'")
 endif()
 
 # Check for supported compilers
-set(CXX_IS_GNU OFF)
-set(CXX_IS_CLANG OFF)
-set(CXX_IS_MSVC OFF)
-set(CXX_IS_VALID ON)
-
-if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    set(CXX_IS_GNU ON)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     message(STATUS "Compiler ID is: GNU")
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CXX_IS_CLANG ON)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     message(STATUS "Compiler ID is: Clang")
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-    set(CXX_IS_MSVC ON)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     message(STATUS "Compiler ID is: MSVC")
 else()
-    set(CXX_IS_VALID OFF)
-endif()
-
-# Check for supported compilers
-if (NOT (CXX_IS_VALID))
     message(FATAL_ERROR "Your compiler: '"
         ${CMAKE_CXX_COMPILER_ID}
         "' is not supported")
--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,20 +1,20 @@
 function(SetupSharedLibrary)
     # GCC compiler, mostly for linux
-    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         set(CMAKE_CXX_FLAGS "-Wall -ansi -Wno-deprecated -pthread -fmax-errors=3" CACHE STRING "" FORCE)
         set(CMAKE_CXX_FLAGS_DEBUG "-g -ggdb -O0 -DLOG_ENABLED" CACHE STRING "" FORC)
         set(CMAKE_CXX_FLAGS_RELEASE "-O3 -march=native -DNDEBUG" CACHE STRING "" FORCE)
     endif()
 
     # Clang, mostly for Mac
-    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         set(CMAKE_CXX_FLAGS "-Wall -Wno-deprecated" CACHE STRING "" FORCE)
         set(CMAKE_CXX_FLAGS_DEBUG "-g -ggdb -O0 -DLOG_ENABLED" CACHE STRING "" FORCE)
         set(CMAKE_CXX_FLAGS_RELEASE "-O3 -march=native -DNDEBUG" CACHE STRING "" FORCE)
     endif()
 
     # MSVC, for windows
-    if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON CACHE BOOL "" FORCE)
         set(CMAKE_CXX_FLAGS "/Wall /DBOOST_ALL_NO_LIB /EHsc" CACHE STRING "" FORCE)
         set(CMAKE_CXX_FLAGS_DEBUG "/Zi /Od -DLOG_ENABLED" CACHE STRING "" FORCE)
