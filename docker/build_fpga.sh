56c7b0df2ba7dfa536e9c3dd221cc05c4d64cfb8
Ben Varkey Benjamin <benvarkey@nazgul>
2017-09-28 02:27:08 -0700

    Cleaning up Docker setup

--- docker/build_fpga.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOSTIP=$(ip route show | awk '/default/ {print $3}')
-JENKINS_HOST_PATH=$(echo "$1" | sed -e 's/^\/var/\/home\/jenkins/g')
-ssh $HOSTIP "docker run --rm -i -v /home/quartus:/home/quartus -v ${JENKINS_HOST_PATH}:/pystorm $2"
