2cbf12fda73f9470452a02092077ccfd337f192e
aneckar <aneckar@stanford.edu>
2017-10-17 14:35:33 -0700

    Redesigned upstream FPGA handshaker
    not actually sure that there was anything wrong with the old one, though

--- FPGA/src/FPGA_BD_Interface.sv
+++ FPGA/src/FPGA_BD_Interface.sv
@@ -8,6 +8,11 @@ module FPGA_TO_BD #(parameter NUM_BITS = `NUM_BITS_PIN2CORE)
                     output reg valid, output reg [NUM_BITS-1:0] data,
                     input ready, reset, clk);
 
+// XXX this has a bug right now: needs to keep track of which
+// half-cycle BD currently is in. Otherwise, metastability is possible.
+// solved for now by making this clock have the same frequency as BD IO clk
+// consequence is half throughput
+
 /*
 input bd_channel.d  // Data read from bd_channel (from the FPGA core)
 input bd_channel.v  // Asserted if bd_channel data is valid
@@ -106,75 +111,65 @@ module BD_TO_FPGA #(parameter NUM_BITS = `NUM_BITS_CORE2PIN)
                    (Channel bd_channel,
                     input valid, input [NUM_BITS-1:0] data,
                     output reg ready, input reset, clk);
-    /*
-    output channel.d  // Data sent to channel
-    output channel.v  // Assert if valid data to send
-    input channel.a   // At reset 0, asserted if channel.d is latched
-    output ready;     // BD sends data if ready is asserted (sync)
-    input valid;      // If asserted, data is valid (sync)
-    input data;       // Registered data from BD (sync)
-    input reset;      // Global reset (async)
-    input clk;        // Global clock
-    */
+  /*
+  output channel.d  // Data sent to channel
+  output channel.v  // Assert if valid data to send
+  input channel.a   // At reset 0, asserted if channel.d is latched
+  output ready;     // BD sends data if ready is asserted (sync)
+  input valid;      // If asserted, data is valid (sync)
+  input data;       // Registered data from BD (sync)
+  input reset;      // Global reset (async)
+  input clk;        // Global clock
+  */
 
-    /*
-    The various states are
-      bd_channel.a  |  valid  |  bd_channel.v  ||  bd_channel.v  |  ready
-            0       |    0    |        0       ||        0       |    1
-            0       |    0    |        1       ||        1       |    0
-            1       |    0    |        x       ||        0       |    1
-            0       |    1    |        x       ||        1       |    0
-            1       |    1    |        x       ||        1       |    0
-    */
+  enum {READY_HIGH, READY_LOW} state, next_state;
 
-    reg slow_valid;
+  logic [NUM_BITS-1:0] data_int, next_data_int;
 
-    typedef enum {
-      BOTH_OFF = 0,
-      FPGA_OFF = 1,
-      BD_OFF   = 2,
-      BOTH_ON  = 3} stateType;
-
-    stateType state;
-    assign state = stateType'({bd_channel.a, valid});
-
-    always_ff @(posedge clk, posedge reset)
-    begin
-        if(reset == 1)
-        begin
-            slow_valid     <= 0;
-            ready          <= 1;
-        end
-        else
-        begin
-            case(state)
-                //BOTH_OFF: begin
-                //    bd_channel.v   <= bd_channel.v;
-                //    ready          <= ~bd_channel.v;
-                //end
-                FPGA_OFF: begin
-                    ready          <= 0;
-                    if(ready)
-                    begin
-                        slow_valid     <= 1;
-                        bd_channel.d   <= data;
-                    end
-                end
-                BD_OFF: begin
-                    slow_valid     <= 0;
-                    ready          <= 1;
-                end
-                BOTH_ON: begin
-                    ready          <= 0;
-                    if(ready)
-                    begin
-                        slow_valid     <= 1;
-                        bd_channel.d   <= data;
-                    end
-                end
-            endcase
-        end
+  always_ff @(posedge clk, posedge reset) 
+    if (reset == 1) begin
+      state <= READY_HIGH; 
+      data_int <= '0;
+    end
+    else begin
+      state <= next_state;
+      data_int <= next_data_int;
     end
 
-    assign bd_channel.v = (valid & ready & bd_channel.a) ? 1 : slow_valid;
+  always_comb
+    unique case (state)
+      READY_HIGH: 
+        if (valid == 1) begin // data_int will be sampled, ack
+          next_state = READY_LOW; 
+          next_data_int = data;
+        end
+        else begin
+          next_state = READY_HIGH;
+          next_data_int = data_int;
+        end
+      READY_LOW: 
+        if (bd_channel.a == 1) begin // can proceed if FPGA side acks
+          next_state = READY_HIGH;
+          next_data_int = data_int;
+        end
+        else begin
+          next_state = READY_LOW;
+          next_data_int = data_int;
+        end
+    endcase
+
+  always_comb
+    unique case (state)
+      READY_HIGH: begin
+        ready = 1;
+        bd_channel.v = 0;
+      end
+      READY_LOW: begin
+        ready = 0;
+        bd_channel.v = 1;
+      end
+    endcase
+
+  assign bd_channel.d = data_int;
+
 endmodule
