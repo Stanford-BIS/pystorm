da151afd76d0a9575bf7987c68a398cad2d8bac3
aneckar <aneckar@stanford.edu>
2017-10-20 18:09:31 -0700

    Wrote core_pars for BD

--- pystorm/hal/neuromorph/core_pars.py
+++ pystorm/hal/neuromorph/core_pars.py
@@ -1,12 +1,12 @@
 def get_core_pars():
     return dict(
-            MM_height=80,
-            MM_width=80,
-            AM_size=160,
-            TAT_size=320,
+            MM_height=256,
+            MM_width=256,
+            AM_size=1024,
+            TAT_size=1024,
             NeuronArray_height=64,
             NeuronArray_width=64,
-            NeuronArray_pool_size=4,
+            NeuronArray_pool_size=64,
             num_threshold_levels=8,
             min_threshold_value=64,
             max_weight_value=127,
