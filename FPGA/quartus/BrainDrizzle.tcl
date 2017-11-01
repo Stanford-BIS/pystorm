5e01a90e58dae7e1d028d91c628678ad92c737c8
alexcarsello <alex.carsello@gmail.com>
2017-10-31 18:43:28 -0700

    Initial router commit

---
+++ FPGA/quartus/BrainDrizzle.tcl
@@ -0,0 +1,91 @@
+# Copyright (C) 2017  Intel Corporation. All rights reserved.
+# Your use of Intel Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Intel Program License 
+# Subscription Agreement, the Intel Quartus Prime License Agreement,
+# the Intel MegaCore Function License Agreement, or other 
+# applicable license agreement, including, without limitation, 
+# that your use is for the sole purpose of programming logic 
+# devices manufactured by Intel and sold by Intel or its 
+# authorized distributors.  Please refer to the applicable 
+# agreement for further details.
+
+# Quartus Prime: Generate Tcl File for Project
+# File: BrainDrizzle.tcl
+# Generated on: Tue Oct 31 17:47:32 2017
+
+# Load Quartus Prime Tcl Project package
+package require ::quartus::project
+
+set need_to_close_project 0
+set make_assignments 1
+
+# Check that the right project is open
+if {[is_project_open]} {
+	if {[string compare $quartus(project) "BrainDrizzle"]} {
+		puts "Project BrainDrizzle is not open"
+		set make_assignments 0
+	}
+} else {
+	# Only open if not already open
+	if {[project_exists BrainDrizzle]} {
+		project_open -revision BrainDrizzle BrainDrizzle
+	} else {
+		project_new -revision BrainDrizzle BrainDrizzle
+	}
+	set need_to_close_project 1
+}
+
+# Make assignments
+if {$make_assignments} {
+	set_global_assignment -name FAMILY "MAX 10"
+	set_global_assignment -name DEVICE 10M08SCU169A7G
+	set_global_assignment -name ORIGINAL_QUARTUS_VERSION 17.0.0
+	set_global_assignment -name PROJECT_CREATION_TIME_DATE "12:33:44  OCTOBER 13, 2017"
+	set_global_assignment -name LAST_QUARTUS_VERSION "17.0.0 Lite Edition"
+	set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
+	set_global_assignment -name MIN_CORE_JUNCTION_TEMP "-40"
+	set_global_assignment -name MAX_CORE_JUNCTION_TEMP 125
+	set_global_assignment -name DEVICE_FILTER_PACKAGE UFBGA
+	set_global_assignment -name DEVICE_FILTER_PIN_COUNT 169
+	set_global_assignment -name ERROR_CHECK_FREQUENCY_DIVISOR 256
+	set_global_assignment -name PARTITION_NETLIST_TYPE SOURCE -section_id Top
+	set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id Top
+	set_global_assignment -name PARTITION_COLOR 16764057 -section_id Top
+	set_global_assignment -name POWER_PRESET_COOLING_SOLUTION "23 MM HEAT SINK WITH 200 LFPM AIRFLOW"
+	set_global_assignment -name POWER_BOARD_THERMAL_MODEL "NONE (CONSERVATIVE)"
+	set_global_assignment -name FLOW_ENABLE_POWER_ANALYZER ON
+	set_global_assignment -name POWER_DEFAULT_INPUT_IO_TOGGLE_RATE "12.5 %"
+	set_global_assignment -name SYSTEMVERILOG_FILE ../src/router/InputController.sv
+	set_global_assignment -name SYSTEMVERILOG_FILE ../src/router/BrainDrizzle.sv
+	set_global_assignment -name SYSTEMVERILOG_FILE ../src/router/allocator.sv
+	set_instance_assignment -name PARTITION_HIERARCHY root_partition -to | -section_id Top
+
+	# Including default assignments
+	set_global_assignment -name TIMEQUEST_MULTICORNER_ANALYSIS ON -family "MAX 10"
+	set_global_assignment -name TIMEQUEST_REPORT_WORST_CASE_TIMING_PATHS OFF -family "MAX 10"
+	set_global_assignment -name TIMEQUEST_CCPP_TRADEOFF_TOLERANCE 0 -family "MAX 10"
+	set_global_assignment -name TDC_CCPP_TRADEOFF_TOLERANCE 0 -family "MAX 10"
+	set_global_assignment -name TIMEQUEST_DO_CCPP_REMOVAL ON -family "MAX 10"
+	set_global_assignment -name TIMEQUEST_SPECTRA_Q OFF -family "MAX 10"
+	set_global_assignment -name SYNTH_TIMING_DRIVEN_SYNTHESIS ON -family "MAX 10"
+	set_global_assignment -name SYNCHRONIZATION_REGISTER_CHAIN_LENGTH 2 -family "MAX 10"
+	set_global_assignment -name SYNTH_RESOURCE_AWARE_INFERENCE_FOR_BLOCK_RAM ON -family "MAX 10"
+	set_global_assignment -name OPTIMIZE_HOLD_TIMING "ALL PATHS" -family "MAX 10"
+	set_global_assignment -name OPTIMIZE_MULTI_CORNER_TIMING ON -family "MAX 10"
+	set_global_assignment -name AUTO_DELAY_CHAINS ON -family "MAX 10"
+	set_global_assignment -name CRC_ERROR_OPEN_DRAIN OFF -family "MAX 10"
+	set_global_assignment -name USE_CONFIGURATION_DEVICE ON -family "MAX 10"
+	set_global_assignment -name ENABLE_OCT_DONE ON -family "MAX 10"
+
+	# Commit assignments
+	export_assignments
+
+	# Close project
+	if {$need_to_close_project} {
+		project_close
+	}
+}
---
+++ FPGA/src/router/InputController.sv
@@ -0,0 +1,119 @@
+//Controls input FIFOs
+
+module InputController (
+input clk,
+input almost_empty,
+input ready_0,
+input ready_1,
+input [10:0] data_in,
+output reg req_0,
+output reg req_1,
+output reg read,
+output reg [10:0] data_out
+);
+
+wire tail;
+wire [9:0] dest;
+//SUBJECT TO CHANGE. HAVE TO DECIDE ON DATA FORMAT
+assign dest=data_in[9:0];
+assign tail = data_in[10];
+////////////////////////////////////////////
+
+reg [1:0] state=2'b00;  
+reg [1:0] next_state;
+parameter idle = 2'b00; 
+parameter parse = 2'b01; 
+parameter send_0 = 2'b10;
+parameter send_1 = 2'b11;
+
+//Next State combinational logic
+always @ (state or tail or almost_empty or data_in or ready_0 or ready_1)
+begin : next_state_logic
+	next_state = state;
+	case(state)
+		idle:		begin
+						if(almost_empty==0)
+							next_state=parse;
+					end	
+						
+		parse:	begin
+						if($signed(dest)>0 && ready_1==1) begin
+							next_state=send_1;
+						end
+						else if ($signed(dest)<=0 && ready_0==1) begin
+							next_state=send_0;
+						end
+					end
+							
+		send_0:	begin
+						if((tail==1) && (almost_empty==1)) begin
+							next_state=idle;
+						end
+						else if((tail==1) && (almost_empty==0)) begin
+							next_state=parse;
+						end
+					end
+		
+		send_1:	begin
+						if((tail==1) && (almost_empty==1)) begin
+							next_state=idle;
+						end
+						else if((tail==1) && (almost_empty==0)) begin
+							next_state=parse;
+						end
+					end
+		
+		default:	next_state=state;
+	
+	endcase
+end
+
+always @ (posedge clk)
+	begin: state_register
+	state <= next_state;
+end
+
+//output combinational logic
+always @ (state or ready_0 or ready_1 or tail or dest or data_in or almost_empty)
+begin: output_logic
+	case(state)
+		idle:		begin
+						read=~almost_empty;
+						req_0=0;
+						req_1=0;
+						data_out=data_in;
+					end
+
+		parse:	begin
+						if($signed(dest) > 0) begin
+							req_0=0;
+							req_1=1;
+							//decrement hop count in header.
+							data_out={data_in[10],dest-10'b1};
+							read=~almost_empty & ready_1;
+						end
+						else begin
+							req_0=1;
+							req_1=0;
+							//negate destination if going out the bottom
+							data_out={data_in[10],-dest};
+							read=~almost_empty & ready_0;
+						end
+							
+					end
+						
+		send_0:	begin
+						req_0=~almost_empty;
+						read = ~almost_empty && ready_0;
+						data_out=data_in;
+					end
+				
+		send_1:	begin
+						req_1=~almost_empty;
+						read = ~almost_empty && ready_1;
+						data_out=data_in;
+					end
+	endcase
+end
+
+endmodule
