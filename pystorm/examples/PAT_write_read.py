7f3cc38f80635268608a91a24ce732cce22ab356
aneckar <aneckar@stanford.edu>
2017-11-03 16:51:57 -0700

    Updated PAT_write_read.py tests, works w/ chip

--- pystorm/examples/PAT_write_read.py
+++ pystorm/examples/PAT_write_read.py
@@ -10,11 +10,24 @@ print("* Sent reset")
 
 PATSize = 64
 PAT_vals = [i for i in range(PATSize)] # typically, you would use PackWord to set memory word values
-D.SetMem(0, bdpars.BDMemId.PAT, PAT_vals, 0)
+D.SetMem(0, bd.bdpars.BDMemId.PAT, PAT_vals, 0)
 print("* Set Memory")
 
-dumped = D.DumpMem(0, bdpars.BDMemId.PAT)
+dumped = D.DumpMem(0, bd.bdpars.BDMemId.PAT)
 print("* Dumped Memory")
+
+print("* Test over!")
+print("we wrote:")
+print(PAT_vals)
+print("we read:")
 print(dumped)
 
+same = [i == j for i,j in zip(PAT_vals, dumped)]
+if(sum(same) == len(PAT_vals)):
+    print("--> test passed!")
+else:
+    print("--> test FAILED!")
+
 D.Stop()
+
+exit(0)
