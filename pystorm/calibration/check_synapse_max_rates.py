e9da101fc748d9c124ed0330124ad82721bbb204
Sam Fok <sambfok@gmail.com>
2018-02-02 16:13:32 -0800

    switch from continuous to discrete max rate sweep
    fpga spike rates are quantized

--- pystorm/calibration/check_synapse_max_rates.py
+++ pystorm/calibration/check_synapse_max_rates.py
@@ -1,10 +1,11 @@
-"""Find the maximum firing rate of the synapses as driven by bias
+"""Find the maximum input firing rate of the synapses
 
-Set synapse offset bias to max
-Set synapse refractory bias to max (minimum refractory)
-
-Iterate through the synapses, and check for FIFO overflow
-Plot results
+For each synapse:
+Set up the Tag Action Table to send +1 and -1 spikes to an individual synapse for each input spike
+generated from one of the FPGA's spike generators.
+Do a binary search over the spike generator's output rate.
+For each rate: check if the FIFO buffer overflows, which indicates that spikes are being sent
+faster than the synapse can be consumed
 """
 import os
 import sys
@@ -22,13 +23,11 @@ from pystorm.PyDriver import bddriver as bd
 
 CORE = 0
 NRN_N = 4096
-# SYN_N = 64
-SYN_N = 1024
+SYN_N = 1
+# SYN_N = 1024
 
 RUN_TIME = 1.0
 INTER_RUN_TIME = 0.2
-RATE_TOL = 0.001
-BOUND_FINDING_SCALE = 2
 
 SPIKE_GEN_TIME_UNIT_NS = 10000 # time unit of fpga spike generator
 SPIKE_GEN_IDX = 0 # FPGA spike generator index
@@ -41,7 +40,25 @@ TAT_SIGN_1 = 1
 
 SYN_PD_PU = 1024 # analog bias setting
 
-INIT_RATE = 10000 # initial rate to test
+INIT_RATE = 12500 # initial rate to test
+MIN_RATE = 1000 # minimum rate to test
+MAX_RATE = 1000000 # maximum rate to test
+
+def compute_fpga_rates(min_rate, max_rate, spike_gen_time_unit_ns):
+    """Compute the fpga spike generator rates between the min and max rates"""
+    max_spike_gen_time_units = 1./min_rate*1E9/spike_gen_time_unit_ns
+    min_spike_gen_time_units = 1./max_rate*1E9/spike_gen_time_unit_ns
+    max_spike_gen_time_units = int(np.ceil(max_spike_gen_time_units))
+    min_spike_gen_time_units = max(int(np.floor(min_spike_gen_time_units)), 1)
+    periods_spike_gen_time_units = np.arange(
+        max_spike_gen_time_units, min_spike_gen_time_units-1, -1)
+    fpga_rates = 1./(periods_spike_gen_time_units*SPIKE_GEN_TIME_UNIT_NS*1E-9)
+    fpga_rates = np.ceil(fpga_rates).astype(int)
+    return fpga_rates
+
+SPIKE_GEN_RATES = compute_fpga_rates(MIN_RATE, MAX_RATE, SPIKE_GEN_TIME_UNIT_NS)
+INIT_RATE_IDX = np.argmin(np.abs(SPIKE_GEN_RATES-INIT_RATE))
+N_RATES = len(SPIKE_GEN_RATES)
 
 DATA_DIR = "./data/" + os.path.basename(__file__)[:-3] + "/"
 if not os.path.isdir(DATA_DIR):
@@ -75,7 +92,7 @@ def set_analog():
 def set_hal():
     """Set the HAL traffic settings"""
     # clear queues
-    HAL.set_time_resolution(downstream_ns=SPIKE_GEN_TIME_UNIT_NS)
+    HAL.set_time_resolution(downstream_ns=SPIKE_GEN_TIME_UNIT_NS, upstream_ns=10000000)
     HAL.start_traffic(flush=False)
     HAL.disable_spike_recording(flush=False)
     HAL.disable_output_recording(flush=True)
@@ -105,52 +122,73 @@ def toggle_spk_generator(rate):
 
 def find_syn_max_rate_bounds():
     """Find the minimum and maximum bounds of the max rate"""
-    rate = INIT_RATE
-    upper_rate = None
-    lower_rate = None
+    idx = INIT_RATE_IDX
+    upper_idx = None
+    lower_idx = None
+    d_idx = 1
 
-    overflow = toggle_spk_generator(rate)
+    overflow = toggle_spk_generator(SPIKE_GEN_RATES[idx])
     if overflow:
-        upper_rate = rate
+        upper_idx = idx
     else:
-        lower_rate = rate
+        lower_idx = idx
 
-    while not lower_rate:
-        rate //= BOUND_FINDING_SCALE
-        overflow = toggle_spk_generator(rate)
+    while not lower_idx:
+        prev_idx = idx
+        idx -= d_idx
+        if idx < 0:
+            if prev_idx > 0:
+                idx = 0
+            else:
+                break
+        overflow = toggle_spk_generator(SPIKE_GEN_RATES[idx])
         if overflow:
-            upper_rate = rate
+            upper_idx = idx
+            d_idx *= 2
         else:
-            lower_rate = rate
+            lower_idx = idx
 
-    while not upper_rate:
-        rate *= BOUND_FINDING_SCALE
-        overflow = toggle_spk_generator(rate)
+    while not upper_idx:
+        prev_idx = idx
+        idx += d_idx
+        if idx >= N_RATES:
+            if prev_idx < N_RATES-1:
+                idx = N_RATES-1
+            else:
+                break
+        overflow = toggle_spk_generator(SPIKE_GEN_RATES[idx])
         if overflow:
-            upper_rate = rate
+            upper_idx = idx
         else:
-            lower_rate = rate
-    return lower_rate, upper_rate
+            lower_idx = idx
+            d_idx *= 2
 
-def measure_syn_max_rate():
+    return lower_idx, upper_idx
+
+def check_syn_max_rate():
     """Deliver spikes to a pair of synapses to find the minimum of their maximum input rates"""
-    lower_rate, upper_rate = find_syn_max_rate_bounds()
+    lower_idx, upper_idx = find_syn_max_rate_bounds()
 
-    while (upper_rate-lower_rate)/lower_rate > RATE_TOL:
-        rate = int((upper_rate+lower_rate)/2.)
-        overflow = toggle_spk_generator(rate)
+    if not lower_idx:
+        return -1, True
+    elif not upper_idx:
+        return N_RATES-1, True
+
+    while upper_idx-lower_idx > 1:
+        idx = int(np.round((upper_idx+lower_idx)/2.))
+        overflow = toggle_spk_generator(SPIKE_GEN_RATES[idx])
         if overflow:
-            upper_rate = rate
+            upper_idx = idx
         else:
-            lower_rate = rate
+            lower_idx = idx
 
     # check again
-    of_upper = toggle_spk_generator(upper_rate)
-    of_lower = toggle_spk_generator(lower_rate)
-    bound_check = of_upper > 0 and of_lower == 0
+    overflow_upper = toggle_spk_generator(SPIKE_GEN_RATES[idx])
+    overflow_lower = toggle_spk_generator(SPIKE_GEN_RATES[idx])
+    bound_check = overflow_upper > 0 and overflow_lower == 0
     # due to TAT configuration, 2 spikes sent to synapse per spike to TAT
-    max_rate = upper_rate+lower_rate
-    return max_rate, bound_check
+    max_idx = lower_idx
+    return max_idx, bound_check
 
 def plot_max_rates(max_rates):
     """Plot the data"""
@@ -213,6 +251,15 @@ def plot_max_rates(max_rates):
     fig_2d_surf.savefig(DATA_DIR + "2d_surface.pdf")
     fig_hist.savefig(DATA_DIR + "histogram.pdf")
 
+def report_time_remaining(start_time, syn_idx):
+    """Occasionally estimate and report the remaining time"""
+    if syn_idx%4 == 0:
+        n_syn_completed = syn_idx+1
+        delta_time = get_time()-start_time
+        est_time_remaining = delta_time/(syn_idx+1) * (SYN_N-n_syn_completed)
+        print("estimated time remaining: {:.0f} s = {:.1f} min = {:.2f} hr...".format(
+            est_time_remaining, est_time_remaining/60., est_time_remaining/60./60.))
+
 def check_synapse_max_rates(parsed_args):
     """Run the check"""
     use_saved_data = parsed_args.use_saved_data
@@ -223,6 +270,9 @@ def check_synapse_max_rates(parsed_args):
             print("\nError: Could not find saved data {}\n".format(DATA_DIR + "max_rates.txt"))
             sys.exit(1)
     else:
+        print("Checking maximum synaptic input rates over possible input rates \n" +
+              str(SPIKE_GEN_RATES) +
+              "\nStarting with rate {}".format(SPIKE_GEN_RATES[INIT_RATE_IDX]))
         build_net()
         set_analog()
         set_hal()
@@ -231,16 +281,12 @@ def check_synapse_max_rates(parsed_args):
         start_time = get_time()
         for syn_idx in range(SYN_N):
             set_tat(syn_idx)
-            rate, check = measure_syn_max_rate()
-            max_rates[syn_idx] = rate
+            rate_idx, check = check_syn_max_rate()
+            max_rates[syn_idx] = SPIKE_GEN_RATES[rate_idx]
             bound_checks[syn_idx] = check
-            print("syn:{} max_rate:{:.0f} bounds_consistent:{}".format(syn_idx, rate, check))
-            if syn_idx%4 == 0:
-                n_syn_completed = syn_idx+1
-                delta_time = get_time()-start_time
-                est_time_remaining = delta_time/(syn_idx+1) * (SYN_N-n_syn_completed)
-                print("estimated time remaining: {:.0f} s = {:.1f} min = {:.2f} hr...".format(
-                    est_time_remaining, est_time_remaining/60., est_time_remaining/60./60.))
+            print("syn:{} max_rate:{:.0f} bounds_consistent:{}".format(
+                syn_idx, max_rates[syn_idx], check))
+            report_time_remaining(start_time, syn_idx)
         print("Max firing rates:")
         print(max_rates)
         print("Min synapse spike consumption times:")
@@ -248,7 +294,7 @@ def check_synapse_max_rates(parsed_args):
         print("Bound consistency checks:")
         print(bound_checks)
 
-    plot_max_rates(max_rates)
+    # plot_max_rates(max_rates)
     if not use_saved_data:
         np.savetxt(DATA_DIR + "max_rates.txt", max_rates)
     plt.show()
