34c6f985a69fea31733011946f583e5d7cc79eb9
noza <noza@stanford.edu>
2018-03-07 14:35:25 -0800

    test_clobbering.py is not pytest compatible with this version
    I commented out the pytest compatibility for the time being to keep the
    code pushing through any errors for board debugging purposes.  I was
    tired of stashing and unstashing, so I'm commiting to this repo for now,
    so that the default for testing the bad_grt bits is this version of the
    code.

--- pystorm/test/test_clobbering.py
+++ pystorm/test/test_clobbering.py
@@ -20,8 +20,8 @@ from pystorm.calibration.utils.exp import clear_tags, compute_spike_gen_rates
 CORE_ID = 0 # Set Core to 0, b/c Braindrop only has 1 core
 DIM = 1 # 1 dimensional
 WEIGHT = 1 # weight of connection from input to output
-RUN_TIME = 0.0001 # time to sample
-INTER_RUN_TIME = 2.2 # time between samples
+RUN_TIME = 0.01 # time to sample in s
+INTER_RUN_TIME = 2.2 # time between samples in seconds
 
 MEM_DELAY = 15
 
@@ -32,7 +32,9 @@ TAT_IDX = 0
 TAT_START_ADDR = 0
 
 N_TAGS_TO_SEND = 10
-N_CYCLES = 1
+N_TAGS_TO_SHOW = 10
+N_CYCLES = 3
+IGNORE_TAG_VAL = 2047
 
 DOWNSTREAM_TIME = 100
 
@@ -120,7 +122,7 @@ def toggle_hal(net_input, rate):
     HAL.enable_output_recording(flush=True)
     t0 = HAL.driver.GetFPGATime()
 #    print("FPGA Time: {}".format(t0))
-    fudge = 1000000000
+    fudge = int(1e9) # add a 1 s (1e9 ns) fudge
 #    print("FPGA Time+Fudge: {}".format(t0+fudge))
     HAL.set_input_rate(net_input, 0, rate, time=t0+fudge, flush=True)
 #    HAL.driver.SendTags(CORE_ID, tag_words, tag_times)
@@ -145,10 +147,15 @@ def sweep_tag_input_rates(parsed_args):
 
 #        program_spike_filter()
 #        program_TAT()
-        rates = np.arange(952375, 952391, 1) # specify rates in units of Hz
+        rates = np.arange(800000, 900000, 10000) # specify rates in units of Hz
+        rates = np.arange(100000, 900000, 100000) # specify rates in units of Hz
+        rates = np.array([769230, 980000])
+#        rates = np.logspace(4.0, 6.0, num=5).astype(int)
         n_rates = len(rates)
         print("Checking {} rates: {}".format(n_rates, rates))
+#        measured_rates = np.zeros(n_rates)
         measured_rates = np.zeros(N_CYCLES*n_rates)
+#        measured_rates_tags = np.zeros(n_rates)
         measured_rates_tags = np.zeros(N_CYCLES*n_rates)
 
         HAL.driver.SetSpikeFilterDebug(CORE_ID, True)
@@ -164,29 +171,25 @@ def sweep_tag_input_rates(parsed_args):
 #                measured_time = (binned_tags[-1, 0] - binned_tags[0, 0])/1e9
 #                #print("measured time: {}".format(measured_time))
 #                total_tags = np.sum(binned_tags[:, 3])
-#                measured_rates[cycle_idx*len(rates)+rate_idx] = total_tags/measured_time
+#                measured_rates[rate_idx] = total_tags/measured_time
+#                #measured_rates[cycle_idx*len(rates)+rate_idx] = total_tags/measured_time
 #                filtered_tags = binned_tags[binned_tags[:, 3] > 0]
 #                print("Output Tags:")
 #                print(filtered_tags[:, 1:4])
-#                print("[OUTPUT] [{}]\tCount of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 3])))
-#                print("[OUTPUT] [{}]\tDims of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 2])))
+#                for tag_idx, cur_output in enumerate(net_outputs):
+#                    cur_total_tags = np.sum(filtered_tags[filtered_tags[:, 1] == tag_idx, 0])
+#                    print("[OUTPUT] [{}]\tCount of output [{}]\t".format(rate, tag_idx)+
+#                          "tags: {} ({:5.2f}%)".format(cur_total_tags, cur_total_tags/total_tags*100))
 #                print("[OUTPUT] [{}]\tNumber of output tags: {}".format(rate, total_tags))
-#                print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates[cycle_idx*len(rates)+rate_idx]))
+#                print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates[rate_idx]))
+#                #print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates[cycle_idx*len(rates)+rate_idx]))
 
                 # Use RecvTags instead of get_outputs
-#                sf_ids, sf_states, sf_times = toggle_spk_generator(rate)
-#                print("[OUTPUT] Spike Filter Ids: {}".format(sf_ids[sf_states!=0]))
-#                print("[OUTPUT] Spike Filter Counts: {}".format(sf_states[sf_states!=0]))
-#                print("[OUTPUT] Sum Spike Filter Counts: {}".format(np.sum(sf_states)))
-
                 tag_outputs = np.array(HAL.driver.RecvUnpackedTags(CORE_ID, 1000)).T
+                #print("[OUTPUT] [{}] Sample of raw tag outputs: {}".format(rate, tag_outputs[:12]))
                 filtered_tags = tag_outputs[tag_outputs[:, 0] > 0]
+                #print("[OUTPUT] [{}] Sample of filtered tag outputs: {}".format(rate, tag_outputs[:12]))
+
                 tag_counts = filtered_tags[:, 0]
                 tag_tags = filtered_tags[:, 1]
                 #tag_routes = filtered_tags[:, 2]
@@ -201,25 +204,46 @@ def sweep_tag_input_rates(parsed_args):
                 else:
                     measured_time = 1e9
                 total_tags = len(tag_tags)
+                #measured_rates_tags[rate_idx] = 1.*total_tags/measured_time
                 measured_rates_tags[cycle_idx*len(rates)+rate_idx] = 1.*total_tags/measured_time
 
-                print("\nReceived Tags:")
-                print("[OUTPUT] [{}]\t{}".format(rate, tag_tags[:40]))
+                print("\nReceived Tags for Rate {}:".format(rate))
 
                 for tag_idx, cur_output in enumerate(net_outputs):
                     cur_total_tags = np.sum(filtered_tags[filtered_tags[:, 1] == tag_idx, 0])
-                    print("[OUTPUT] [{}]\tCount of output [{}] tags: {} ({:5.2f}%)".format(rate, tag_idx, cur_total_tags, cur_total_tags/total_tags*100))
-#                total_tags1 = np.sum(filtered_tags[filtered_tags[:, 1] == 1, 0])
-#                total_tags2 = np.sum(filtered_tags[filtered_tags[:, 1] == 2, 0])
-#                total_tags3 = np.sum(filtered_tags[filtered_tags[:, 1] == 3, 0])
-#                print("[OUTPUT] [{}]\tCount of output [0] tags: {} ({:5.2f})".format(rate, total_tags0, total_tags0/total_tags*100))
-#                print("[OUTPUT] [{}]\tCount of output [1] tags: {} ({:5.2f})".format(rate, total_tags1, total_tags1/total_tags*100))
-#                print("[OUTPUT] [{}]\tCount of output [2] tags: {} ({:5.2f})".format(rate, total_tags2, total_tags2/total_tags*100))
-#                print("[OUTPUT] [{}]\tCount of output [3] tags: {} ({:5.2f})".format(rate, total_tags3, total_tags3/total_tags*100))
-                print("[OUTPUT] [{}]\tMeasured time: {}\tNum of tags: {}\tSum of tag counts: {}".format(rate, measured_time, total_tags, np.sum(tag_counts)))
-                print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates_tags[cycle_idx*len(rates)+rate_idx]))
+                    print("[OUTPUT] [{}]\tCount of output [{}]\t".format(rate, tag_idx)+
+                          "tags: {} ({:5.2f}%)".format(cur_total_tags, cur_total_tags/total_tags*100))
+                print("[OUTPUT] [{}]\tMeasured time: {}\t".format(rate, measured_time) +
+                      "Num of tags: {}\tSum of tag counts: {}".format(total_tags, np.sum(tag_counts)))
+                print("[OUTPUT] [{}]\t".format(rate) +
+                      #"Measured rate: {}".format( measured_rates_tags[rate_idx]))
+                      "Measured rate: {}".format( measured_rates_tags[cycle_idx*len(rates)+rate_idx]))
 
-                print("[OUTPUT] [{}] total overflows: {}\n".format(rate, HAL.get_overflow_counts()))
+                print("[OUTPUT] [{}]\tTotal overflows: {}\n".format(rate, HAL.get_overflow_counts()))
+                print("[OUTPUT] [{}]\t{}\n".format(rate, tag_tags[:N_TAGS_TO_SEND*4]))
+
+#                # Assert test for automatic pytest compatibility
+#                non_padded_tag_idx = 0
+#                input_tag_list = np.array(range(N_TAGS_TO_SEND))
+#                for tag_i, tag in enumerate(tag_tags):
+#                    if tag == IGNORE_TAG_VAL:
+#                        #print("Ignored a padded tag output entry")
+#                        pass
+#                    else:
+#                        cur_tag_idx = non_padded_tag_idx % N_TAGS_TO_SEND
+#                        if tag_i < N_TAGS_TO_SHOW:
+#                            assert input_tag_list[cur_tag_idx] == tag, (
+#                                "Received unexpected tag value of {} ".format(tag) +
+#                                "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
+#                                "Tag Stream starting @ index 0:\n\t" +
+#                                "{}".format(tag_tags[0:tag_i+N_TAGS_TO_SHOW]))
+#                        else:
+#                            assert input_tag_list[cur_tag_idx] == tag, (
+#                                "Received unexpected tag value of {} ".format(tag) +
+#                                "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
+#                                "Tag Stream centered @ index {}:\n\t".format(tag_i) +
+#                                "{}".format(tag_tags[tag_i-N_TAGS_TO_SHOW:tag_i+N_TAGS_TO_SHOW+1]))
+#                        non_padded_tag_idx += 1
 
 
 if __name__ == "__main__":
