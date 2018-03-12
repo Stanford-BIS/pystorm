5b773456f291020335ed42c89b5c6d87ddc7bafd
Sam Fok <samfok@stanford.edu>
2018-03-12 12:45:43 -0700

    sticky clobber test cleaning

--- pystorm/test/test_clobbering.py
+++ pystorm/test/test_clobbering.py
@@ -5,18 +5,14 @@ spike generator -> TAT -> output
 
 Vary the input spike rates and see if output packets get clobbered
 """
-import os
 import time
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-np.set_printoptions(precision=1)
-
 from pystorm.hal import HAL
 from pystorm.hal.neuromorph import graph # to describe HAL/neuromorph network
 from pystorm.PyDriver import bddriver as bd
-from pystorm.calibration.utils.exp import clear_tags, compute_spike_gen_rates
+from pystorm.calibration.utils.exp import clear_tags
 HAL = HAL()
+np.set_printoptions(precision=1)
 
 CORE_ID = 0 # Set Core to 0, b/c Braindrop only has 1 core
 DIM = 1 # 1 dimensional
@@ -38,17 +34,6 @@ N_CYCLES = 1
 IGNORE_TAG_VAL = 2047
 
 DOWNSTREAM_TIME = 100
-
-DATA_DIR = "./data/" + os.path.basename(__file__)[:-3] + "/"
-if not os.path.isdir(DATA_DIR):
-    os.makedirs(DATA_DIR, exist_ok=True)
-
-def parse_args():
-    """Parse command line arguments"""
-    parser = argparse.ArgumentParser(description='Characterize the soma max firing rates')
-    parser.add_argument("-r", action="store_true", dest="use_saved_data", help='reuse cached data')
-    args = parser.parse_args()
-    return args
 
 def set_memory_delays(delay):
     HAL.driver.SetMemoryDelay(CORE_ID, bd.bdpars.BDMemId.PAT, delay, delay)
@@ -137,114 +122,110 @@ def toggle_hal(net_input, rate):
     time.sleep(INTER_RUN_TIME)
 #    HAL.set_time_resolution(upstream_ns=1000000, downstream_ns=DOWNSTREAM_TIME)
 
-def sweep_tag_input_rates(parsed_args):
+def test_clobbering():
     """Perform the test"""
-    use_saved_data = parsed_args.use_saved_data
-    if use_saved_data:
-        print("Should be using saved data, but does nothing in this script at the moment")
-    else:
-        HAL.disable_spike_recording(flush=True)
-#        HAL.set_time_resolution(upstream_ns=100000, downstream_ns=DOWNSTREAM_TIME)
+    HAL.disable_spike_recording(flush=True)
+#    HAL.set_time_resolution(upstream_ns=100000, downstream_ns=DOWNSTREAM_TIME)
 
-#        program_spike_filter()
-#        program_TAT()
-        rates = np.arange(952375, 952391, 1) # specify rates in units of Hz
-        n_rates = len(rates)
-        print("Checking {} rates: {}".format(n_rates, rates))
-        measured_rates = np.zeros(N_CYCLES*n_rates)
-        measured_rates_tags = np.zeros(N_CYCLES*n_rates)
+#    program_spike_filter()
+#    program_TAT()
+    rates = np.arange(952375, 952391, 1) # specify rates in units of Hz
+    n_rates = len(rates)
+    print("Checking {} rates: {}".format(n_rates, rates))
+    measured_rates = np.zeros(N_CYCLES*n_rates)
+    measured_rates_tags = np.zeros(N_CYCLES*n_rates)
 
-        HAL.driver.SetSpikeFilterDebug(CORE_ID, True)
-        net_input, net_outputs = build_net()
+    HAL.driver.SetSpikeFilterDebug(CORE_ID, True)
+    net_input, net_outputs = build_net()
 
-        for cycle_idx in range(N_CYCLES):
-            for rate_idx, rate in enumerate(rates):
-                toggle_hal(net_input, rate)
-#                # Use get_outputs
-#                print("\n[OUTPUT] Checking rate {}".format(rate))
-#                binned_tags = HAL.get_outputs()
-#                #print("[OUTPUT] Shape of outputs: {}".format(binned_tags.shape))
-#                measured_time = (binned_tags[-1, 0] - binned_tags[0, 0])/1e9
-#                #print("measured time: {}".format(measured_time))
-#                total_tags = np.sum(binned_tags[:, 3])
-#                measured_rates[cycle_idx*len(rates)+rate_idx] = total_tags/measured_time
-#                filtered_tags = binned_tags[binned_tags[:, 3] > 0]
-#                print("Output Tags:")
-#                print(filtered_tags[:, 1:4])
-#                print("[OUTPUT] [{}]\tCount of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 3])))
-#                print("[OUTPUT] [{}]\tCount of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 3])))
-#                print("[OUTPUT] [{}]\tDims of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 2])))
-#                print("[OUTPUT] [{}]\tDims of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 2])))
-#                print("[OUTPUT] [{}]\tNumber of output tags: {}".format(rate, total_tags))
-#                print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates[cycle_idx*len(rates)+rate_idx]))
+    for cycle_idx in range(N_CYCLES):
+        for rate_idx, rate in enumerate(rates):
+            toggle_hal(net_input, rate)
+#            # Use get_outputs
+#            print("\n[OUTPUT] Checking rate {}".format(rate))
+#            binned_tags = HAL.get_outputs()
+#            #print("[OUTPUT] Shape of outputs: {}".format(binned_tags.shape))
+#            measured_time = (binned_tags[-1, 0] - binned_tags[0, 0])/1e9
+#            #print("measured time: {}".format(measured_time))
+#            total_tags = np.sum(binned_tags[:, 3])
+#            measured_rates[cycle_idx*len(rates)+rate_idx] = total_tags/measured_time
+#            filtered_tags = binned_tags[binned_tags[:, 3] > 0]
+#            print("Output Tags:")
+#            print(filtered_tags[:, 1:4])
+#            print("[OUTPUT] [{}]\tCount of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 3])))
+#            print("[OUTPUT] [{}]\tCount of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 3])))
+#            print("[OUTPUT] [{}]\tCount of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 3])))
+#            print("[OUTPUT] [{}]\tCount of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 3])))
+#            print("[OUTPUT] [{}]\tDims of output [0] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output1, 2])))
+#            print("[OUTPUT] [{}]\tDims of output [1] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output2, 2])))
+#            print("[OUTPUT] [{}]\tDims of output [2] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output3, 2])))
+#            print("[OUTPUT] [{}]\tDims of output [3] tags: {}".format(rate, np.sum(filtered_tags[filtered_tags[:, 1] == net_output4, 2])))
+#            print("[OUTPUT] [{}]\tNumber of output tags: {}".format(rate, total_tags))
+#            print("[OUTPUT] [{}]\tMeasured rate: {}".format(rate, measured_rates[cycle_idx*len(rates)+rate_idx]))
 
-                # Use RecvTags instead of get_outputs
-#                sf_ids, sf_states, sf_times = toggle_spk_generator(rate)
-#                print("[OUTPUT] Spike Filter Ids: {}".format(sf_ids[sf_states!=0]))
-#                print("[OUTPUT] Spike Filter Counts: {}".format(sf_states[sf_states!=0]))
-#                print("[OUTPUT] Sum Spike Filter Counts: {}".format(np.sum(sf_states)))
+            # Use RecvTags instead of get_outputs
+#            sf_ids, sf_states, sf_times = toggle_spk_generator(rate)
+#            print("[OUTPUT] Spike Filter Ids: {}".format(sf_ids[sf_states!=0]))
+#            print("[OUTPUT] Spike Filter Counts: {}".format(sf_states[sf_states!=0]))
+#            print("[OUTPUT] Sum Spike Filter Counts: {}".format(np.sum(sf_states)))
 
-                tag_outputs = np.array(HAL.driver.RecvUnpackedTags(CORE_ID, 1000)).T
-                filtered_tags = tag_outputs[tag_outputs[:, 0] > 0]
-                tag_counts = filtered_tags[:, 0]
-                tag_tags = filtered_tags[:, 1]
-                #tag_routes = filtered_tags[:, 2]
-                tag_times = filtered_tags[:, 3]
-                #print("[OUTPUT] [{}] Sample of tag counts: {}".format(rate, tag_counts[:12]))
-                #print("[OUTPUT] [{}] Sample of tag tags: {}".format(rate, tag_tags[:12]))
-                #print("[OUTPUT] [{}] Sample of tag routes: {}".format(rate, tag_routes[:12]))
-                #print("[OUTPUT] [{}] Sample of tag times: {}".format(rate, tag_times[:12]))
+            tag_outputs = np.array(HAL.driver.RecvUnpackedTags(CORE_ID, 1000)).T
+            filtered_tags = tag_outputs[tag_outputs[:, 0] > 0]
+            tag_counts = filtered_tags[:, 0]
+            tag_tags = filtered_tags[:, 1]
+            #tag_routes = filtered_tags[:, 2]
+            tag_times = filtered_tags[:, 3]
+            #print("[OUTPUT] [{}] Sample of tag counts: {}".format(rate, tag_counts[:12]))
+            #print("[OUTPUT] [{}] Sample of tag tags: {}".format(rate, tag_tags[:12]))
+            #print("[OUTPUT] [{}] Sample of tag routes: {}".format(rate, tag_routes[:12]))
+            #print("[OUTPUT] [{}] Sample of tag times: {}".format(rate, tag_times[:12]))
 
-                # Get and print output information
-                if(len(tag_times) > 0):
-                    measured_time = (tag_times[-1] - tag_times[0])/1e9
+            # Get and print output information
+            if tag_times.shape[0] > 0:
+                measured_time = (tag_times[-1] - tag_times[0])/1e9
+            else:
+                measured_time = 1e9
+            total_tags = len(tag_tags)
+            measured_rates_tags[cycle_idx*len(rates)+rate_idx] = 1.*total_tags/measured_time
+
+            print("\nReceived Tags for Rate {}:".format(rate))
+            #print("[OUTPUT] [{}]\t{}".format(rate, tag_tags[:N_TAGS_TO_SEND*4]))
+
+            for tag_idx, cur_output in enumerate(net_outputs):
+                cur_total_tags = np.sum(filtered_tags[filtered_tags[:, 1] == tag_idx, 0])
+                print("[OUTPUT] [{}]\tCount of output [{}]\t".format(rate, tag_idx)+
+                      "tags: {} ({:5.2f}%)".format(cur_total_tags, cur_total_tags/total_tags*100))
+            print("[OUTPUT] [{}]\tMeasured time: {}\t".format(rate, measured_time) +
+                  "Num of tags: {}\tSum of tag counts: {}".format(total_tags, np.sum(tag_counts)))
+            print("[OUTPUT] [{}]\t".format(rate) +
+                  "Measured rate: {}".format(measured_rates_tags[cycle_idx*len(rates)+rate_idx]))
+
+            print("[OUTPUT] [{}]\tTotal overflows: {}\n".format(rate, HAL.get_overflow_counts()))
+
+            # Assert test for automatic pytest compatibility
+            non_padded_tag_idx = 0
+            input_tag_list = np.array(range(N_TAGS_TO_SEND))
+            for tag_i, tag in enumerate(tag_tags):
+                if tag == IGNORE_TAG_VAL:
+                    #print("Ignored a padded tag output entry")
+                    pass
                 else:
-                    measured_time = 1e9
-                total_tags = len(tag_tags)
-                measured_rates_tags[cycle_idx*len(rates)+rate_idx] = 1.*total_tags/measured_time
-
-                print("\nReceived Tags for Rate {}:".format(rate))
-                #print("[OUTPUT] [{}]\t{}".format(rate, tag_tags[:N_TAGS_TO_SEND*4]))
-
-                for tag_idx, cur_output in enumerate(net_outputs):
-                    cur_total_tags = np.sum(filtered_tags[filtered_tags[:, 1] == tag_idx, 0])
-                    print("[OUTPUT] [{}]\tCount of output [{}]\t".format(rate, tag_idx)+
-                          "tags: {} ({:5.2f}%)".format(cur_total_tags, cur_total_tags/total_tags*100))
-                print("[OUTPUT] [{}]\tMeasured time: {}\t".format(rate, measured_time) +
-                      "Num of tags: {}\tSum of tag counts: {}".format(total_tags, np.sum(tag_counts)))
-                print("[OUTPUT] [{}]\t".format(rate) +
-                      "Measured rate: {}".format( measured_rates_tags[cycle_idx*len(rates)+rate_idx]))
-
-                print("[OUTPUT] [{}]\tTotal overflows: {}\n".format(rate, HAL.get_overflow_counts()))
-
-                # Assert test for automatic pytest compatibility
-                non_padded_tag_idx = 0
-                input_tag_list = np.array(range(N_TAGS_TO_SEND))
-                for tag_i, tag in enumerate(tag_tags):
-                    if tag == IGNORE_TAG_VAL:
-                        #print("Ignored a padded tag output entry")
-                        pass
+                    cur_tag_idx = non_padded_tag_idx % N_TAGS_TO_SEND
+                    if tag_i < N_TAGS_TO_SHOW:
+                        assert input_tag_list[cur_tag_idx] == tag, (
+                            "Received unexpected tag value of {} ".format(tag) +
+                            "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
+                            "Tag Stream starting @ index 0:\n\t" +
+                            "{}".format(tag_tags[0:tag_i+N_TAGS_TO_SHOW]))
                     else:
-                        cur_tag_idx = non_padded_tag_idx % N_TAGS_TO_SEND
-                        if tag_i < N_TAGS_TO_SHOW:
-                            assert input_tag_list[cur_tag_idx] == tag, (
-                                "Received unexpected tag value of {} ".format(tag) +
-                                "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
-                                "Tag Stream starting @ index 0:\n\t" +
-                                "{}".format(tag_tags[0:tag_i+N_TAGS_TO_SHOW]))
-                        else:
-                            assert input_tag_list[cur_tag_idx] == tag, (
-                                "Received unexpected tag value of {} ".format(tag) +
-                                "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
-                                "Tag Stream centered @ index {}:\n\t".format(tag_i) +
-                                "{}".format(tag_tags[tag_i-N_TAGS_TO_SHOW:tag_i+N_TAGS_TO_SHOW+1]))
-                        non_padded_tag_idx += 1
+                        assert input_tag_list[cur_tag_idx] == tag, (
+                            "Received unexpected tag value of {} ".format(tag) +
+                            "(expected tag value of {}) at index {}\n".format(input_tag_list[cur_tag_idx], tag_i) +
+                            "Tag Stream centered @ index {}:\n\t".format(tag_i) +
+                            "{}".format(tag_tags[tag_i-N_TAGS_TO_SHOW:tag_i+N_TAGS_TO_SHOW+1]))
+                    non_padded_tag_idx += 1
 
 
 
 if __name__ == "__main__":
-    sweep_tag_input_rates(parse_args())
+    test_clobbering()
--- pystorm/test/test_sticky_bits.py
+++ pystorm/test/test_sticky_bits.py
@@ -6,17 +6,13 @@ set memory with known pattern (all zeros, then all ones) -> read memory for same
 If the output from the written memory doesn't have the same values as the input,
 , then we can assume there is a problem with a sticky bit in the output.
 """
-import os
 import time
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-np.set_printoptions(precision=1)
-
 from pystorm.hal import HAL
 from pystorm.hal.neuromorph import graph # to describe HAL/neuromorph network
 from pystorm.PyDriver import bddriver as bd
 HAL = HAL()
+np.set_printoptions(precision=1)
 
 CORE_ID = 0 # Set Core to 0, b/c Braindrop only has 1 core
 DIM = 1 # 1 dimensional
@@ -27,14 +23,6 @@ MEM_TYPE = bd.bdpars.BDMemId.TAT1 # Allow for quick testing of any memory
 MEM_WIDTH = 29  # The number bits in the data of the memory
 NUM_MEM_ENTRIES = 1024    # number of memory entries in the full memory
 N_CYCLES = 1
-
-# parse_args is currently unused, but kept in case I want to use it in the future
-def parse_args():
-    """Parse command line arguments"""
-    parser = argparse.ArgumentParser(description='Check accuracy of outputs for sticky bits')
-    parser.add_argument("-r", action="store_true", dest="use_saved_data", help='reuse cached data')
-    args = parser.parse_args()
-    return args
 
 def set_memory_delays(delay):
     """Set memory delays"""
@@ -58,42 +46,41 @@ def build_net():
     set_memory_delays(MEM_DELAY)
     return net_input
 
-def test_mem(parsed_args):
+def test_sticky_bits():
     """Perform the test"""
-    use_saved_data = parsed_args.use_saved_data
-    if use_saved_data:
-        print("Should be using saved data, but does nothing in this script at the moment")
-    else:
-        for cycle in range(N_CYCLES):
-            # Set memory delay
-            set_memory_delays(MEM_DELAY)
+    for _ in range(N_CYCLES):
+        # Set memory delay
+        set_memory_delays(MEM_DELAY)
 
-            # Set memory with alternating bits of all 0s and all 1s every entry
-            MemEntries = [0,2**MEM_WIDTH-1]*int(NUM_MEM_ENTRIES/2) # Set memory to alternate between all 0s and all 1s
-            HAL.driver.SetMem(CORE_ID, MEM_TYPE, MemEntries, 0)
+        # Set memory with alternating bits of all 0s and all 1s every entry
+        mem_entries = [0, 2**MEM_WIDTH-1]*int(NUM_MEM_ENTRIES/2)
+        HAL.driver.SetMem(CORE_ID, MEM_TYPE, mem_entries, 0)
 
-            # Allow some time for memory to finish writing  (Do I need to do this?)
-            time.sleep(SLEEP_TIME)
+        # Allow some time for memory to finish writing  (Do I need to do this?)
+        time.sleep(SLEEP_TIME)
 
-            # Dump memory num_dump_times over and over
-            num_repeat_range = 1
-            print("Memory Dump:")
-            for i in range(num_repeat_range):
-                mem_dump = np.array(HAL.driver.DumpMem(CORE_ID, MEM_TYPE))
-                mem_range = len(mem_dump)
-                for i in range(int(mem_range/2)):
-                    mem_entry1 = mem_dump[i*2]
-                    mem_entry2 = mem_dump[i*2+1]
-                    print("{:04}: {:0{width}b}\t{:04}: {:0{width}b}".format(i*2, mem_entry1, i*2+1, mem_entry2, width=MEM_WIDTH))
+        # Dump memory num_dump_times over and over
+        num_repeat_range = 1
+        print("Memory Dump:")
+        for _ in range(num_repeat_range):
+            mem_dump = np.array(HAL.driver.DumpMem(CORE_ID, MEM_TYPE))
+            mem_range = len(mem_dump)
+            for idx in range(int(mem_range/2)):
+                mem_entry1 = mem_dump[idx*2]
+                mem_entry2 = mem_dump[idx*2+1]
+                print("{:04}: {:0{width}b}\t".format(idx*2, mem_entry1, width=MEM_WIDTH) +
+                      "{:04}: {:0{width}b}".format(idx*2+1, mem_entry2, width=MEM_WIDTH))
 
-#            # Dump just one entry in memory over and over again
-#            mem_range_start = 5
-#            num_repeat_range = 10
-#            for i in range(num_repeat_range):
-#                mem_dump = np.array(HAL.driver.DumpMemRange(CORE_ID, MEM_TYPE, mem_range_start, mem_range_start+1))
-#                mem_range = len(mem_dump)
-#                print("{:02}: {:0{width}b}".format(mem_range_start, mem_dump[0], width=MEM_WIDTH))
+#        # Dump just one entry in memory over and over again
+#        mem_range_start = 5
+#        num_repeat_range = 10
+#        for i in range(num_repeat_range):
+#            mem_dump = np.array(HAL.driver.DumpMemRange(
+#                CORE_ID, MEM_TYPE, mem_range_start, mem_range_start+1))
+#            mem_range = len(mem_dump)
+#            print("{:02}: {:0{width}b}".format(mem_range_start, mem_dump[0], width=MEM_WIDTH))
+
+        # TODO Add assertions to check for correctness
 
 if __name__ == "__main__":
-    test_mem(parse_args())
-
+    test_sticky_bits()
