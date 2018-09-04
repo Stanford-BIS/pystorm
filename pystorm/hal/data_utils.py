2d7fa05ba432904f55a15938ab92d65c1bc49061
Sam Fok <sambfok@gmail.com>
2018-09-04 00:23:56 -0700

    isi histogram

--- pystorm/hal/data_utils.py
+++ pystorm/hal/data_utils.py
@@ -10,3 +10,12 @@ def lpf(signal, tau, dt):
     for idx in range(1, len(signal)):
         ret[idx] += ret[idx-1]*decay
     return ret
+
+def bin_to_spk_times(bins, bin_times):
+    """Convert binned spikes to spike times"""
+    ubin_vals = np.unique(bins)[1:]
+    spk_times = []
+    for uval in ubin_vals:
+        spk_times += list(bin_times[np.nonzero(bins==uval)[0]])
+    spk_times = np.sort(np.array(spk_times))
+    return spk_times
