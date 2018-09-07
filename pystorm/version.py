3f52c67207c59df41ec1f2279b9e882555400a87
Sam Fok <sambfok@gmail.com>
2018-09-07 16:25:28 -0700

    init versioning info
    Versioning will help pystorm clients aware of API changes
    if used as intended by symantic versioning (https://semver.org/)
    
    Version info described as MAJOR.MINOR.PATCH
    Changes in MAJOR indicate API breaking changes so that pystorm
    clients can detect and make changes accordingly.
    Changes in MINOR and PATCH indicate changes that do not break client
    APIs

---
+++ pystorm/version.py
@@ -0,0 +1,6 @@
+"""pystorm version information. We use semantic versioning (see http://semver.org/)."""
+
+name = "pystorm"
+version_info = (1, 0, 0)  # (major, minor, patch)
+
+version = "{v}".format(v='.'.join(str(v) for v in version_info))
